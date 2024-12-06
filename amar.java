c839997bdebd8b843b18b32dab1eaf36fb939be5
Amarendar <165167935+amar9100@users.noreply.github.com>
2024-12-06 21:17:38 +0530

    Update amar.java

--- amar.java
+++ amar.java
@@ -1,1 +1,1 @@
-public class amar{public static void main(String args[]){System.out.println("this is for testing")}}
+public class amar{public static void main(String args[]){System.out.println("this is for testing");}
